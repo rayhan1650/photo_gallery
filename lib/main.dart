a661f98baa4bce576c461791ab766394b1666770
Rayhan <nm.rayhan.1650@gmail.com>
2024-05-03 16:42:37 +0600

    appbar created

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(primaryColor: Color(0xff2CAB00)),
       title: 'Photo Gallery',
       home: Home(),
     );
@@ -22,11 +24,50 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Photo Gallery',
+          style: TextStyle(
+              color: Color(0xffEFEFEF),
+              fontSize: 20,
+              fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
+        leading: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: InkWell(
+            splashColor: Colors.transparent,
+            onTap: () {
+              // SystemNavigator.pop();
+            },
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: Colors.white38,
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: const Center(
+                child: Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.more_vert,
+                color: Color(0xffEFEFEF),
+              ))
+        ],
+        backgroundColor: Color(0xff2CAB00),
+      ),
       body: Center(
         child: Text('Photo Gallery'),
       ),
     );
   }
 }
-
-
